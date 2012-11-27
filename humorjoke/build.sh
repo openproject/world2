673e4a99ee867e0867b34988903f9996c261d1cb
jayfeng <jayfeng@jubuntu.(none)>
2012-11-27 15:06:24 +0800

    remove call phone permission and upgrade build.sh to v2.0

--- humorjoke/build.sh
+++ humorjoke/build.sh
@@ -2,12 +2,12 @@
 basedir=$(cd "$(dirname "$0")";pwd)
 cd $basedir
 
-markets="google appchina waps gfan 91 hiapk goapk mumayi eoe nduo feiliu crossmo huawei qq 3g 360 baidu sohu 163 samsung coolmart meizu moto xiaomi lenovo nearme official dev"
-#markets="google official dev"
+#markets="google official appchina waps gfan 91 hiapk goapk mumayi eoe nduo feiliu crossmo huawei qq 3g 360 baidu sohu 163 samsung coolmart meizu moto xiaomi lenovo nearme dev"
+markets="google hiapk official dev"
 for market in $markets
 do
-    echo packaging humorjoke_1.1_$market.apk ...
+    echo packaging humorjoke_2.0_$market.apk ...
     sed -i "s/\(android:value=\)\"\(.*\)\"\( android:name=\"UMENG_CHANNEL\"\)/\1\"$market\"\3/g" AndroidManifest.xml
     sed -i "s/\(android:value=\)\"\(.*\)\"\( android:name=\"WAPS_PID\"\)/\1\"$market\"\3/g" AndroidManifest.xml
-    ant -Dapk-name=humorjoke -Dapk-version=v1.1 -Dapk-market=$market
+    ant -Dapk-name=humorjoke -Dapk-version=v2.0 -Dapk-market=$market
 done
