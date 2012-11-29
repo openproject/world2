36545177ba35dc45d5b6af2f95de6eb78d8c1a30
jayfeng <jayfeng@jubuntu.(none)>
2012-11-29 23:02:17 +0800

    upgrade playenglish build.sh to v2.0

--- playenglish/build.sh
+++ playenglish/build.sh
@@ -2,12 +2,12 @@
 basedir=$(cd "$(dirname "$0")";pwd)
 cd $basedir
 
-#markets="google appchina waps gfan 91 hiapk goapk mumayi eoe nduo feiliu crossmo huawei qq 3g 360 baidu sohu 163 samsung coolmart meizu moto xiaomi lenovo nearme official dev"
-markets="waps dev"
+markets="google appchina waps gfan 91 hiapk goapk mumayi eoe nduo feiliu crossmo huawei qq 3g 360 baidu sohu 163 samsung coolmart meizu moto xiaomi lenovo nearme official dev"
+#markets="waps dev"
 for market in $markets
 do
-    echo packaging playenglish_1.0_beta_$market.apk ...
+    echo packaging playenglish_2.0_$market.apk ...
     sed -i "s/\(android:value=\)\"\(.*\)\"\( android:name=\"UMENG_CHANNEL\"\)/\1\"$market\"\3/g" AndroidManifest.xml
     sed -i "s/\(android:value=\)\"\(.*\)\"\( android:name=\"WAPS_PID\"\)/\1\"$market\"\3/g" AndroidManifest.xml
-    ant -Dapk-name=playenglish -Dapk-version=v1.0_beta -Dapk-market=$market
+    ant -Dapk-name=playenglish -Dapk-version=v2.0 -Dapk-market=$market
 done
