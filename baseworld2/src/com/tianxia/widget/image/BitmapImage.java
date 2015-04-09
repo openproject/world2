bf54d82c8f67f364bcdbaa0f0703b517121d0caf
henry.zhang <henry.zhang@sagesoft.cn>
2015-04-09 11:38:07 +0800

    test
    test

--- baseworld2/src/com/tianxia/widget/image/BitmapImage.java
+++ baseworld2/src/com/tianxia/widget/image/BitmapImage.java
@@ -2,6 +2,7 @@ package com.tianxia.widget.image;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.widget.TextView;
 
 public class BitmapImage implements SmartImage {
     private Bitmap bitmap;
